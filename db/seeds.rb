a3dbd8619a209e84ef5967df888f32c23bdd3636
Grant Thomas <grantwilliamthomas@hotmail.co.uk>
2019-08-14 16:39:29 +0200

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting DB"
+Restaurant.destroy_all
+
+puts "Seeding DB with Restaurants..."
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      'London',
+    phone_number:  '555 555 555',
+    category:        "french"
+  },
+  {
+    name:         'Ottolenghi',
+    address:      'London',
+    phone_number:  '555 555 556',
+    category:        "french"
+  },
+  {
+    name:         'Gazzo',
+    address:      'Berlin',
+    phone_number:  '455 555 555',
+    category:       "french"
+  },
+  {
+    name:         'La Lucha',
+    address:      'Berlin',
+    phone_number:  '455 555 555',
+    category:        "french"
+  },{
+    name:         'Riza',
+    address:      'Berlin',
+    phone_number:  '455 555 555',
+    category:      "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
